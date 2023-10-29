394b49ab847c25d2429966e85268338d35d560b3
IAMMO <iammothedev@gmail.com>
2023-10-29 19:37:18 +0100

    + added keycode depencendy + refactored code + added YAKC logo

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const { app, Tray, Menu, BrowserWindow, ipcMain, screen } = require("electron");
 const iohook = require("@mechakeys/iohook");
 const fs = require("fs");
 const keycodemap = require("./keycode/index");
+const keycode = require("keycode");
+let config;
 
 app.on("ready", () => {
   // Load Config
-  const config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
+  config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
   const {
     showOnMonitor,
     position,
@@ -20,8 +22,6 @@ app.on("ready", () => {
 
   // Select the monitor in the config if available
   const selectedMonitor = displays[showOnMonitor] || screen.getPrimaryDisplay();
-
-  console.log(displays[showOnMonitor]);
 
   const window = new BrowserWindow({
     x: selectedMonitor.bounds.x,
@@ -47,38 +47,19 @@ app.on("ready", () => {
   window.loadFile("./index.html");
   window.webContents.send("config", config);
 
-  if (config && config.selectMap) {
-    keycodemap.setMap(config.selectMap);
-  } else {
-    keycodemap.setMap("us");
-  }
-
   iohook.start();
 
   iohook.on("keydown", (event) => {
-    // TODO: onlyKeysWithModifiers
-
-    let keyLabel = keycodemap.map(event.rawcode);
-
-    if (keyLabel == "SPACE") {
-      if (config && config.showSpaceAsUnicode) {
-        keyLabel = "␣";
-      } else {
-        keyLabel = " ";
-      }
-    }
-
-    if (keyLabel) {
-      window.webContents.send("keydown", keyLabel);
-    }
+    if (convertSpecialKeys(event, config).length > 0)
+      window.webContents.send("keydown", convertSpecialKeys(event, config));
   });
 
   // Create a tray icon
-  let menuTray = new Tray("./assets/testIcon.png");
+  let menuTray = new Tray("./assets/yakc-logo.png");
 
   // Set the context menu
   const contextMenu = Menu.buildFromTemplate([
-    { label: "Quit", click: () => app.quit() },
+    { label: "Quit", click: () => app.exit() },
   ]);
 
   // Set Tool Tip
@@ -93,3 +74,49 @@ ipcMain.on("config", (event, newConfig) => {
     window.webContents.send("config", newConfig);
   }
 });
+
+function convertSpecialKeys(event, config) {
+  const keyLabel = keycode(event.rawcode);
+  const isModifierKey = config && config.onlyKeysWithModifiers;
+  const isShiftKey = event.shiftKey;
+  const isCtrlKey = event.ctrlKey;
+  const isAltKey = event.altKey;
+  const isMetaKey = event.metaKey;
+  const isSpaceKey = keyLabel === "space";
+
+  if (isModifierKey && !isCtrlKey && !isShiftKey && !isAltKey && !isMetaKey) {
+    return "";
+  }
+
+  if (isCtrlKey && keyLabel !== undefined) {
+    return ` CTRL + ${keyLabel.toUpperCase()} `;
+  }
+
+  if (isShiftKey) {
+    if (isModifierKey && keyLabel !== undefined) {
+      return ` SHIFT + ${keyLabel.toUpperCase()} `;
+    }
+
+    if (/^[a-zA-Z]$/.test(keyLabel)) {
+      return keyLabel.toUpperCase();
+    }
+
+    if (keyLabel !== undefined) {
+      return keyLabel;
+    }
+  }
+
+  if (isAltKey && keyLabel !== undefined) {
+    return ` ALT + ${keyLabel.toUpperCase()} `;
+  }
+
+  if (isMetaKey && keyLabel !== undefined) {
+    return ` META + ${keyLabel.toUpperCase()} `;
+  }
+
+  if (isSpaceKey) {
+    return config && config.showSpaceAsUnicode ? "␣" : " ";
+  }
+
+  return keyLabel || "";
+}
